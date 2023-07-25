a1a084e30c72a13c1835cde2f475ede02dbe1ebe
vinoth <vino@gmail.com>
2023-07-25 21:54:59 +0530

    new

--- app.py
+++ app.py
@@ -1,46 +1,26 @@
+from flask import Flask, jsonify
 import requests
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-NOTION_KEY = 'secret_PEaWTQb12wSvcDdVKnSuNeufcHFIS6Ltod1bUs1JpRf'
-NOTION_RESUME_DATABASE_ID = 'f922d1a720d0472ebfa56c2e25154e01'
-NOTION_BASE_URL = 'https://api.notion.com/v1/databases/{}/query'.format(NOTION_RESUME_DATABASE_ID)
-HEADERS = {
-    'Authorization': 'Bearer {}'.format(NOTION_KEY),
-    'Content-Type': 'application/json',
-    'Notion-Version': '2021-08-16',
-}
-@app.route('/')
-def home():
-    return "Welcome to the home page!"
 
+@app.route('/consultancy_database', methods=['GET'])
+def get_consultancy_database():
+    url = "https://api.notion.com/v1/databases/a197366da7b1482d981f52d0e433a750"
+    headers = {
+        'Authorization': 'Bearer secret_Mzoz4xIINL6LBasTElU4cVikpdm18NtHaZzxHWB5CXQ',
+        'Notion-Version': '2022-06-28',
+        'accept': 'application/json'
+    }
 
-@app.route('/company', methods=['GET'])
-def get_companies_by_title():
-    title = request.args.get('title')
-    if not title:
-        return jsonify({'error': 'Company title is required.'}), 400
+    response = requests.get(url, headers=headers)
 
-    # Make a request to the Notion API to fetch data from the database
-    response = requests.post(NOTION_BASE_URL, headers=HEADERS, json={
-        "filter": {
-            "property": "Title",
-            "text": {
-                "contains": title
-            }
-        }
-    })
-
-    # Process the response data and return filtered companies
     if response.status_code == 200:
         data = response.json()
-        filtered_companies = [company['properties'] for company in data['results']]
-        return jsonify(filtered_companies)
+        return jsonify(data)
     else:
-        return jsonify({'error': 'Failed to fetch data from Notion database.'}), 500
+        return jsonify({"message": "Error occurred while fetching data"}), response.status_code
 
-# ... Other routes and functions ...
 
 if __name__ == '__main__':
     app.run(debug=True)
