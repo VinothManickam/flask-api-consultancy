833ef7502f15b8b7f3590f42279ccf1a1e747d1f
vinoth <vino@gmail.com>
2023-07-26 21:44:19 +0530

    End point

--- app.py
+++ app.py
@@ -1,40 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-# Fetch sensitive information from environment variables
-NOTION_KEY = 'secret_Mzoz4xIINL6LBasTElU4cVikpdm18NtHaZzxHWB5CXQ'
-NOTION_RESUME_DATABASE_ID = 'a197366da7b1482d981f52d0e433a750'
+load_dotenv()
 
 
-def fetch_consultancy_database():
-    url = f"https://api.notion.com/v1/databases/{NOTION_RESUME_DATABASE_ID}"
-    headers = {
-        'Authorization': f'Bearer {NOTION_KEY}',
-        'Notion-Version': '2022-06-28',
-        'accept': 'application/json'
-    }
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        return None
+NOTION_KEY = os.environ.get("NOTION_KEY")
+NOTION_RESUME_DATABASE_ID = os.environ["NOTION_RESUME_DATABASE_ID"]
 
 
-@app.route('/consultancy_database', methods=['GET'])
-def get_consultancy_database():
-    data = fetch_consultancy_database()
-    if data:
-        return jsonify(data)
-    else:
-        return jsonify({"message": "Error occurred while fetching data"}), 500
+# ... (Your existing code remains unchanged)
 
-
-def fetch_jobs_offered():
-    url = f"https://api.notion.com/v1/databases/Vwqw/query"
+def fetch_companies_by_consultancy_name(consultancy_name):
+    url = f"https://api.notion.com/v1/databases/{NOTION_RESUME_DATABASE_ID}/query"
     headers = {
         'Authorization': f'Bearer {NOTION_KEY}',
         'Notion-Version': '2022-06-28',
@@ -42,22 +23,63 @@ def fetch_jobs_offered():
         'accept': 'application/json'
     }
 
-    response = requests.post(url, headers=headers, json={})
+    payload = {
+        "filter": {
+            "property": "ConsultancyName",
+            "rich_text": {
+                "contains": consultancy_name
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
     if response.status_code == 200:
         data = response.json()
         return data
     else:
         return None
 
+def fetch_companies_by_jobs_offered(jobs_offered):
+    url = f"https://api.notion.com/v1/databases/{NOTION_RESUME_DATABASE_ID}/query"
+    headers = {
+        'Authorization': f'Bearer {NOTION_KEY}',
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
 
-@app.route('/jobs_offered', methods=['GET'])
-def get_jobs_offered():
-    data = fetch_jobs_offered()
+    payload = {
+        "filter": {
+            "property": "JobsOffered",
+            "rich_text": {
+                "contains": jobs_offered
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+@app.route('/company', methods=['GET'])
+def get_company():
+    consultancy_name = request.args.get('ConsultancyName')
+    jobs_offered = request.args.get('JobsOffered')
+
+    if consultancy_name:
+        data = fetch_companies_by_consultancy_name(consultancy_name)
+    elif jobs_offered:
+        data = fetch_companies_by_jobs_offered(jobs_offered)
+    else:
+        return jsonify({"message": "Invalid query parameters"}), 400
+
     if data:
         return jsonify(data)
     else:
-        return jsonify({"message": "Error occurred while fetching data"}), 500
-
+        return jsonify({"message": "No data found"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
