6b2c878a76f5f5a649e9a3e7bb219422f957ddc6
vinoth <vino@gmail.com>
2023-07-26 18:01:20 +0530

    new

--- app.py
+++ app.py
@@ -3,23 +3,60 @@ import requests
 
 app = Flask(__name__)
 
+# Fetch sensitive information from environment variables
+NOTION_KEY = 'secret_Mzoz4xIINL6LBasTElU4cVikpdm18NtHaZzxHWB5CXQ'
+NOTION_RESUME_DATABASE_ID = 'a197366da7b1482d981f52d0e433a750'
 
-@app.route('/consultancy_database', methods=['GET'])
-def get_consultancy_database():
-    url = "https://api.notion.com/v1/databases/a197366da7b1482d981f52d0e433a750"
+
+def fetch_consultancy_database():
+    url = f"https://api.notion.com/v1/databases/{NOTION_RESUME_DATABASE_ID}"
     headers = {
-        'Authorization': 'Bearer secret_Mzoz4xIINL6LBasTElU4cVikpdm18NtHaZzxHWB5CXQ',
+        'Authorization': f'Bearer {NOTION_KEY}',
         'Notion-Version': '2022-06-28',
         'accept': 'application/json'
     }
 
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
         data = response.json()
+        return data
+    else:
+        return None
+
+
+@app.route('/consultancy_database', methods=['GET'])
+def get_consultancy_database():
+    data = fetch_consultancy_database()
+    if data:
         return jsonify(data)
     else:
-        return jsonify({"message": "Error occurred while fetching data"}), response.status_code
+        return jsonify({"message": "Error occurred while fetching data"}), 500
+
+
+def fetch_jobs_offered():
+    url = f"https://api.notion.com/v1/databases/Vwqw/query"
+    headers = {
+        'Authorization': f'Bearer {NOTION_KEY}',
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+    }
+
+    response = requests.post(url, headers=headers, json={})
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+
+@app.route('/jobs_offered', methods=['GET'])
+def get_jobs_offered():
+    data = fetch_jobs_offered()
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({"message": "Error occurred while fetching data"}), 500
 
 
 if __name__ == '__main__':
